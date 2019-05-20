e8b9f8f7fa18274796cdd164528c0a0295d527a1
Damon <damonhayhurst@gmail.com>
2019-05-20 11:39:31 +0100

    Added better error handling

--- app.js
+++ app.js
@@ -34,3 +34,18 @@ const videoRouter = require('./routes/videoRoutes.js')(nav);
 
 app.use('/lessons', lessonRouter);
 app.use('/videos', videoRouter);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
